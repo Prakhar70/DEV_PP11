f42f1c82f5eb0c545d8d2c278997cbc166b76cb4
prakhar <prakharagarwal70@gmail.com>
2021-04-21 21:05:49 +0530

    g

--- Module1/Lec2_FolderSorting/activity/folderSort.js
+++ Module1/Lec2_FolderSorting/activity/folderSort.js
@@ -43,16 +43,22 @@ function checkExtensionFolder(extension){
     if(!isFolderExist){
         fs.mkdirSync(extensionFolderName);
     }
+    return extensionFolderName;
 }
 
-function moveFile(file , extension){
-
+function moveFile(file , extensionFolderName){
+    let sourceFile = testFolderPath+"/"+file;
+    let destinationFile = extensionFolderName+"/"+file;
+    // copy file from the source path to destination path !!
+    fs.copyFileSync(sourceFile , destinationFile);
+    // then delete file from the source path !!
+    fs.unlinkSync(sourceFile);
 }
 
 
 function sortFile(file){
     let extension = getExtension(file);
     // console.log(extension);
-    checkExtensionFolder(extension);
-    // moveFile();
+    let extensionFolderName = checkExtensionFolder(extension);
+    moveFile(file , extensionFolderName );
 }
