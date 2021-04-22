ed3cb60b1f35a13a14ff9db5acf9c2c3cb05d300
prakhar <prakharagarwal70@gmail.com>
2021-04-22 15:24:10 +0530

    papa

--- Module1/Lec2_FolderSorting/activity/utils.js
+++ Module1/Lec2_FolderSorting/activity/utils.js
@@ -1,5 +1,5 @@
 module.exports={
-    "Documents":["doc","pdf","txt"],
+    "Documents":["doc","pdf","txt","csv","ppt"],
     "Images":["img","jpg","gif","png"],
     "Audio":["mp3"],
     "Video":["mp4","mkv"],
