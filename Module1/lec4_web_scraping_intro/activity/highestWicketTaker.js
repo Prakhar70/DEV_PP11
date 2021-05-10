f34e2810d5cec965ff5692ea411a2b0639afe04d
prakhar <prakharagarwal70@gmail.com>
2021-05-10 16:25:14 +0530

    IPL web scrappimg project

--- Module1/lec4_web_scraping_intro/activity/highestWicketTaker.js
+++ Module1/lec4_web_scraping_intro/activity/highestWicketTaker.js
@@ -5,58 +5,56 @@ const cheerio = require("cheerio");
 
 // request is a high order function
 
-request( matchLink , cb );
-function cb(error , response , data){
+request(matchLink, cb);
+function cb(error, response, data) {
     // console.log("got the data !!!");
     // console.log(data);
     // fs.writeFileSync("./match.html" , data);
     getHighestWicketTaker(data);
 }
-
-
-function getHighestWicketTaker(data){
+function getHighestWicketTaker(data) {
     let myDocument = cheerio.load(data);
     let bothBowlingTables = myDocument(".table.bowler");
-    
+
     // {
     //     "0" : {bowling table} ,
     //     "1" : {bowling table}
     // }
     // console.log(bothBowlingTables);
-    
+
     let highestWicketTakenName;
     let highestWicketsTaken;
     let economyOfHighestWicketTaker;
     // console.log(bothBowlingTables.length);
-    
-    for(let i=0 ; i<bothBowlingTables.length ; i++){
+
+    for (let i = 0; i < bothBowlingTables.length; i++) {
         let bowlingTable = myDocument(bothBowlingTables[i]);
         // fs.writeFileSync("./bowlingTable.html",bowlingTable.html());
         // console.log(bowlingTable);
         let allTableRows = bowlingTable.find("tbody tr")
         // fs.writeFileSync("./bowlingTable.html",allTableRows.html());
         // console.log(allTableRows);
-        
-    //     // {
-    //     //     "0" : {tr},
-    //     //     "1" : {tr},
-    //     //     "2" : {tr}
-    //     // }
-        for(let j=0 ; j<allTableRows.length ; j++){
-             // wicket "4"
+
+        //     // {
+        //     //     "0" : {tr},
+        //     //     "1" : {tr},
+        //     //     "2" : {tr}
+        //     // }
+        for (let j = 0; j < allTableRows.length; j++) {
+            // wicket "4"
             // name "0"
             // economy "5"
             let allTds = myDocument(allTableRows[j]).find("td");
             // {  0 : {} , 1: {} , 2: {}  ,3:{}  }
-            if(i==0 && j==0){
+            if (i == 0 && j == 0) {
                 highestWicketTakenName = myDocument(allTds[0]).find("a").text();
                 highestWicketsTaken = myDocument(allTds[4]).text();
                 economyOfHighestWicketTaker = myDocument(allTds[5]).text();
             }
-            else{
+            else {
                 let currentWickets = myDocument(allTds[4]).text();
                 let currentEconomy = myDocument(allTds[5]).text();
-                if(currentWickets > highestWicketsTaken  || (currentWickets == highestWicketsTaken && currentEconomy < economyOfHighestWicketTaker)){
+                if (currentWickets > highestWicketsTaken || (currentWickets == highestWicketsTaken && currentEconomy < economyOfHighestWicketTaker)) {
                     // update if current bowler have high wickets !!
                     highestWicketTakenName = myDocument(allTds[0]).find("a").text();
                     highestWicketsTaken = currentWickets;
@@ -64,10 +62,10 @@ function getHighestWicketTaker(data){
                 }
             }
         }
-    
+
     }
-    
-    
+
+
     console.log("Name Of Highest Wicket Taker = " + highestWicketTakenName);
     console.log("Wickets Taken = " + highestWicketsTaken)
     console.log("Economy = " + economyOfHighestWicketTaker)
