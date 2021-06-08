7eb954cf8af1427a2d75836d273e86b4ca9a05e7
prakhar <prakharagarwal70@gmail.com>
2021-06-08 21:14:53 +0530

    todo app done

--- Module2/Todo_App/activity/script.js
+++ Module2/Todo_App/activity/script.js
@@ -1,33 +1,49 @@
-let addtodobtn = document.querySelector(".add-todo");
-let inputext = document.querySelector(".todo-input");
-let listcontainer=document.querySelector(".todos-list-container");
-addtodobtn.addEventListener("click", function (e) {
-  if (inputext.value) {
-    addtodo(inputext.value);
-    inputext.value="";
+let addTodoButton = document.querySelector(".add-todo");
+let todoInput = document.querySelector(".todo-input");
+let todosList = document.querySelector(".todos-list-container");
+
+todoInput.addEventListener("keypress", function (e) {
+  if (e.key == "Enter") {
+    addTodo();
   }
 });
-inputext.addEventListener("keypress",function(e){
-    if(inputext.value && e.code=="Enter"){
-        addtodo(inputext.value);
-        inputext.value="";  
-    }
-})
-function addtodo(text){
-    let div = document.createElement("div");
-    div.classList.add("todo-container");
-    let para=document.createElement('p');
-    para.classList.add("todo-para");
-    para.textContent=text;
-    let deletebtn=document.createElement("button");
-    deletebtn.classList.add("delete-btn");
-    deletebtn.textContent="Delete Me";
-    deletebtn.addEventListener("click",deletelist);
-    div.appendChild(para);
-    div.appendChild(deletebtn);
-    listcontainer.appendChild(div);
+addTodoButton.addEventListener("click", function () {
+  addTodo();
+});
+
+// attach click event on addTodoButton
+function addTodo() {
+  let todoInputValue = todoInput.value;
+  if (todoInputValue) {
+    appendTodo(todoInputValue);
+    // it will empty the todoInput
+    todoInput.value = "";
+  }
 }
-function deletelist(e){
-    console.log(e.target.parentNode.remove());
+
+function appendTodo(todo) {
+  let todoItemDiv = document.createElement("div");
+  todoItemDiv.classList.add("todo-item");
+  // <div class="todo-item"> </div>
+
+  let pTag = document.createElement("p");
+  pTag.classList.add("todo");
+  pTag.textContent = todo;
+  // <p class="todo-input">Learn Css</p>
+
+  let deleteTodoButton = document.createElement("button");
+  deleteTodoButton.classList.add("delete-todo");
+  deleteTodoButton.textContent = "Delete";
+  // <button class="delete-todo">Delete</button>
+
+  deleteTodoButton.addEventListener("click", deleteTodo);
+
+  todoItemDiv.append(pTag);
+  todoItemDiv.append(deleteTodoButton);
+
+  todosList.append(todoItemDiv);
 }
-//selfimplemented
+
+function deleteTodo(e) {
+  e.target.parentNode.remove();
+}
