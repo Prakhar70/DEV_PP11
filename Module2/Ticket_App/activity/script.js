c4c5a6bc69ec69d977d3fdf2c8f53e37f382d66b
prakhar <prakharagarwal70@gmail.com>
2021-06-21 16:59:59 +0530

    commit

--- Module2/Ticket_App/activity/script.js
+++ Module2/Ticket_App/activity/script.js
@@ -5,7 +5,7 @@ let openModal = document.querySelector(".open-modal");
 let closeModal = document.querySelector(".close-modal");
 
 let ticketModalOpen = false;
-let isTextInTicketText=false;
+let isTextInTicketText = false;
 
 for (let i = 0; i < allFilters.length; i++) {
   allFilters[i].addEventListener("click", selectFilter);
@@ -25,9 +25,9 @@ function selectFilter(e) {
 }
 
 function openTicketModal(e) {
-  if(ticketModalOpen){
-      return;
-  }  
+  if (ticketModalOpen) {
+    return;
+  }
   let ticketModal = document.createElement("div");
   ticketModal.classList.add("ticket-modal");
   ticketModal.innerHTML = `<div class="ticket-text" contentEditable="true">Enter Your Text!!</div>
@@ -38,46 +38,69 @@ function openTicketModal(e) {
         <div class="ticket-filter yellow"></div>
         <div class="ticket-filter black"></div>
     </div>`;
-    document.querySelector("body").append(ticketModal);
-    
-    let ticketFilters=document.querySelectorAll(".ticket-filter");
-    // console.log(ticketFilters);
-    for(let i=0;i<ticketFilters.length;i++){
-      // console.log(ticketFilters[i]);
-      ticketFilters[i].addEventListener("click",filterclick)
-    }
-    let ticketText=document.querySelector(".ticket-text");
-    ticketText.addEventListener("keypress",handlekeypress);
-    ticketModalOpen = true; 
+  document.querySelector("body").append(ticketModal);
+
+  let ticketFilters = document.querySelectorAll(".ticket-filter");
+  for (let i = 0; i < ticketFilters.length; i++) {
+    ticketFilters[i].addEventListener("click", filterclick);
+  }
+  let ticketText = document.querySelector(".ticket-text");
+  ticketText.addEventListener("keypress", handlekeypress);
+  ticketModalOpen = true;
 }
-function filterclick(e){
-  if(!e.target.classList.contains("selected-filter")){
-      let alreadyselected=document.querySelector(".selected-filter");
-      alreadyselected.classList.remove("selected-filter");
-      e.target.classList.add("selected-filter");
-      
+function filterclick(e) {
+  if (!e.target.classList.contains("selected-filter")) {
+    let alreadyselected = document.querySelector(".selected-filter");
+    alreadyselected.classList.remove("selected-filter");
+    e.target.classList.add("selected-filter");
   }
 }
-function handlekeypress(e){
-  // console.log(isTextInTicketText);
-  // console.log(e);
-  if(e.key=="Enter"){
-    isTextInTicketText=false;
-    // console.log(isTextInTicketText);
+function handlekeypress(e) {
+  if (e.key == "Enter") {
+    isTextInTicketText = false;
+    let selectedcolour =
+      document.querySelector(".selected-filter").classList[1];
+    appendticket(e.target.textContent, selectedcolour);
     closeTicketModal();
     return;
   }
-  if(!isTextInTicketText){
-    // console.log(e.target);
-    e.target.textContent="";
-    
-    
-    isTextInTicketText=true;
+  if (!isTextInTicketText) {
+    e.target.textContent = "";
+    isTextInTicketText = true;
   }
 }
 function closeTicketModal(e) {
-    if(ticketModalOpen){
-        document.querySelector(".ticket-modal").remove();
-        ticketModalOpen  = false;
-    }
+  if (ticketModalOpen) {
+    document.querySelector(".ticket-modal").remove();
+    ticketModalOpen = false;
+  }
+}
+function appendticket(text, color) {
+  console.log(text, color);
+  let carddiv = makeCard(text, color);
+  // console.log(carddiv);
+  add(carddiv);
+}
+function makeCard(text, color) {
+  let createdCardDiv = document.createElement("div");
+  createdCardDiv.classList.add("ticket");
+  createdCardDiv.innerHTML = `
+  <div class="ticket_color_head `+color+`"></div>
+  <div class="ticket_code_delete">
+      <div class="code">#121213123</div>
+      <div class="delete_ticket">
+          <i class="fas fa-trash-alt"></i>
+      </div>
+  </div>
+  <div class="show-ticket-content">`+text+`</div>
+</div>`;
+// console.log(createdCardDiv);
+  return createdCardDiv;
+}
+function add(carddiv) {
+  console.log(carddiv);
+  let tc = document.querySelector(".tickets-container");
+  console.log(tc);
+  
+  tc.append(carddiv);
 }
---
+++ Module2/Ticket_App/activity/test.js
@@ -0,0 +1,3 @@
+var a=10;
+let as=`adasda`+a;
+console.log(as)
